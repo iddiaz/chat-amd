741054fc57285f32a4ed1a674c836d8ccbd6140e
iddiaz <diazdiazivan@gmail.com>
2017-07-03 00:35:47 +0200

    configuracion de requirejs uso de shims para bibliotecas no moduladas

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
-require(['app'],function(app){
+requirejs.config({
+    //mapea el nombre del modulo e indica para que modulos estará disponible
+    //indicar ruta relativa a este archivo.
+    //shim: se usa para las librerias que no son amd y poder usarlas como tal.
+    //Podriamos omitir shim, con la primera conf es suficiente, pero con shim podremos pasar dependencias
+    //en caso de que lo necesite.
+    map: {
+        '*': {
+            'EHH': 'libs/EventHandlerHelper'
+        }
+    },
+    shim: {
+        'EHH': {
+            exports: 'EventHandlerHelper'
+        }
+    }
+});
+
+require(['app', 'EHH'],function(app){
     app.init();
 })
